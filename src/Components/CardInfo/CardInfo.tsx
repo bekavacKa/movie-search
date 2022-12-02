525747634a8f85f6596792b26d23ea4353eb74ab
bekavacKa <th.beka17@gmail.com>
2022-12-02 19:46:25 +0100

    cards info movie/tv show design

---
+++ src/Components/CardInfo/CardInfo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './card-info.scss';
+
+function CardInfo() {
+
+
+  return (
+    <div className='card-info'>
+        <div className='card-info-image'>
+            <img src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c618cd88432989.5dd5e72e505d1.jpg' alt='bla' />
+        </div>
+        <div className='card-info-title'>
+            <h2>seven</h2>        
+        </div>
+    </div>
+  )
+}
+
+export default CardInfo
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import './home.scss';
+import React from "react";
+import "./home.scss";
+import CardInfo from "../../Components/CardInfo/CardInfo";
+
+
 
 function Home() {
+
+
+
   return (
-    <div className='home'>
-      <h2>Find your favorite movies or TV shows</h2>
-      <input type="search" placeholder='Search . . .' className='home-search'/>
-      <div className='home-btns'>
-        <button className='btn movie'> movies </button>
-        <button className='btn tv'> TV Shows </button>
+    <div className="home">
+      <div className="home-title">
+        <h2>Find your favorite movies or TV shows</h2>
       </div>
+
+      <input type="search" placeholder="Search Movie" className="home-search" />
+
+      <div className="home-btns">
+        <button className="btn movie"> movies </button>
+        <button className="btn top"> Top 10 </button>
+        <button className="btn tv active"> TV Shows </button>
+      </div>
+
+      <div className="home-content">
+        <CardInfo />
+
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
